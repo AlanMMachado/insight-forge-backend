bf4acaafa9f661df6e1735c6bb8f2e6c6355a8b3
Alan Machado <sp.alanmarques@gmail.com>
2025-07-06 18:04:02 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/br/edu/fatecgru/insight_forge/InsightForgeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru.insight_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fatecgru/insight_forge/InsightForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru.insight_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
